5aeef47692bd5a985e91a7c22f4324e313165f7c
camille <camille@lzr.pw>
2019-04-19 09:00:24 +0200

    update pi

--- pi.sh
+++ pi.sh
@@ -1,3 +1,3 @@
-for((c=f=10000;d%=f,e=d,b=c-1,g=b*2,c--;h=1)){
-for((;i=h?a[b]:f/5,d=d*b+f*i,a[b--]=d%--g,d/=g,g=b*2,g;)){
-:;};printf %04d $[e+d/f];}
+for((a=b=10000;c=b--;d=1,f=g%a)){
+for((;g=h*c+a*(d?i[c]:a/5),h=g/--k,i[c]=g%k,k=--c*2;)){
+:;};printf %04d $[f+g/a];}
